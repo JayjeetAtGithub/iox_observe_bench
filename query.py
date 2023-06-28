9788f16901bfca5d064093994dd1a68980b025df
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-06-28 09:23:54 -0700

    Benchmark the query time

--- query.py
+++ query.py
@@ -1,5 +1,7 @@
 from flightsql import FlightSQLClient
+
 import pandas
+import time
 
 if __name__ == "__main__":
     client = FlightSQLClient(
@@ -10,7 +12,11 @@ if __name__ == "__main__":
         metadata={"bucket-name": "otel_otel"},
     )
 
-    info = client.execute("select trace_id from spans limit 5;")
+    s = time.time()
+    info = client.execute('SELECT "trace_id", MAX("time") AS t FROM "spans" WHERE spans."service.name" = \'frontend\' AND "time" >= to_timestamp(1687960353941000000) AND "time" <= to_timestamp(1687963953941000000) GROUP BY "trace_id" ORDER BY t DESC LIMIT 1000')
     reader = client.do_get(info.endpoints[0].ticket)
-
-    print(reader.read_all().to_pandas())
+    table = reader.read_all()
+    e = time.time()
+    print(table)
+    print("\n\n")
+    print("Read ", table.num_rows, " rows in ", e - s, " secs")
