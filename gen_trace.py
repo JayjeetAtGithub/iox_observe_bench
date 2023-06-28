4f7c831f1b277dadf474b4258e4579d85108a0d4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-06-28 08:06:21 -0700

    Fix script

--- gen_trace.py
+++ gen_trace.py
@@ -3,15 +3,16 @@ import random
 
 
 def GET_req(url, params):
-    resp = requests.get(url = config_url, params = config_params)
+    print(url)
+    resp = requests.get(url = url, params = params)
     print(resp)
 
 
 if __name__ == "__main__":
     config_url = "http://localhost:8090/config"
-    dispatch_url = "http://localhost:8090/dispatch?customer=392&nonse=0.5107292739627883"
+    dispatch_url = "http://localhost:8090/dispatch"
 
-    for i in range(1, 50000):
+    for i in range(1, 500):
         config_params = {"nonse": random.uniform(1.1, 1.9)}
         GET_req(config_url, config_params)
 
