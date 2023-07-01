17c2dea622f4013437c0fb7a137d67f16ce9e098
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-06-30 18:20:22 -0700

    Show just the query logs

--- start_iox.sh
+++ start_iox.sh
@@ -8,4 +8,4 @@ export INFLUXDB_IOX_ROUTER_HTTP_BIND_ADDR=0.0.0.0:8080
 export INFLUXDB_IOX_QUERIER_GRPC_BIND_ADDR=0.0.0.0:8082
 
 # ../influxdb_iox/target/release/influxdb_iox run all-in-one -vv --catalog-dsn=memory --router-http-bind=0.0.0.0:8080 --querier-grpc-bind=0.0.0.0:8082
-iox/target/release/influxdb_iox run all-in-one -vv
+iox/target/release/influxdb_iox run all-in-one --log-filter iox_query::exec::context=debug
