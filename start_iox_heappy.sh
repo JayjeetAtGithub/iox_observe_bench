cd314cfecc11321d601ba00e56ea6cdf61622280
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-07-16 13:36:08 -0700

    Change print statements

--- start_iox_heappy.sh
+++ start_iox_heappy.sh
@@ -6,4 +6,4 @@ export INFLUXDB_IOX_ROUTER_HTTP_BIND_ADDR=0.0.0.0:8080
 export INFLUXDB_IOX_QUERIER_GRPC_BIND_ADDR=0.0.0.0:8082
 
 cd iox/
-cargo run --no-default-features --features=heappy --profile=release run all-in-one -vv
+cargo run --no-default-features --features=heappy --profile=release run all-in-one --exec-mem-pool-bytes 10737418240 -vv
