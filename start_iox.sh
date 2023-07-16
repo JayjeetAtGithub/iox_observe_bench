cee50bd998275198a74eac4d842eed0e7328c6d6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-07-16 12:54:19 -0700

    Remove -vv from local iox

--- start_iox.sh
+++ start_iox.sh
@@ -5,4 +5,4 @@ echo "Starting IOx"
 export INFLUXDB_IOX_ROUTER_HTTP_BIND_ADDR=0.0.0.0:8080
 export INFLUXDB_IOX_QUERIER_GRPC_BIND_ADDR=0.0.0.0:8082
 
-iox/target/release/influxdb_iox run all-in-one -vv --log-filter iox_query::exec::context=debug
+iox/target/release/influxdb_iox run all-in-one --log-filter iox_query::exec::context=debug
