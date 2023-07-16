db1c618b6c01ace7dc45f5e27969612db29bcb40
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-07-16 12:40:28 -0700

    Add script with heaptrack

--- start_iox_heaptrack.sh
+++ start_iox_heaptrack.sh
@@ -5,4 +5,4 @@ echo "Starting IOx"
 export INFLUXDB_IOX_ROUTER_HTTP_BIND_ADDR=0.0.0.0:8080
 export INFLUXDB_IOX_QUERIER_GRPC_BIND_ADDR=0.0.0.0:8082
 
-iox/target/release/influxdb_iox run all-in-one -vv --log-filter iox_query::exec::context=debug
+cargo with 'heaptrack' -- run --profile=release --no-default-features -- iox/target/release/influxdb_iox run all-in-one -vv
