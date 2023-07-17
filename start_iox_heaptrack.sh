603fb23447f7e47557516a1eceb562dadc9181be
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-07-17 07:53:04 -0700

    Fix heaptrack script

--- start_iox_heaptrack.sh
+++ start_iox_heaptrack.sh
@@ -6,4 +6,4 @@ export INFLUXDB_IOX_ROUTER_HTTP_BIND_ADDR=0.0.0.0:8080
 export INFLUXDB_IOX_QUERIER_GRPC_BIND_ADDR=0.0.0.0:8082
 
 cd iox/
-cargo with 'heaptrack' -- run --profile=release --no-default-features -- ./target/release/influxdb_iox run all-in-one -vv
+cargo with 'heaptrack' -- run --profile=release --no-default-features -- run all-in-one --exec-mem-pool-bytes 10737418240 -vv
